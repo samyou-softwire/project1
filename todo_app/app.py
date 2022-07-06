49ea1b979f41bf21b9e77567b44c1b7fff266593
Samuel Young <sammy.young@softwire.com>
2022-07-06 09:54:20 +0100

    can now update items status

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, reset_items, get_item, save_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -16,3 +16,18 @@ def index():
 def add():
     add_item(request.form.get("title"))
     return redirect(url_for("index"))
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    item = get_item(request.form.get("id"))
+    item['status'] = request.form.get("status")
+    save_item(item)
+
+    return redirect(url_for("index"))
+
+
+@app.route('/reset')
+def reset():
+    reset_items()
+    return "Done!"

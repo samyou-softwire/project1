2dd15847b55e5611f722856417b3ed5524962e01
Samuel Young <sammy.young@softwire.com>
2022-07-06 09:21:44 +0100

    can now add items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     return render_template("index.html", items=get_items())
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    add_item(request.form.get("name"))
+    return redirect(url_for("index"))

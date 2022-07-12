35a611ab8348ec8df59284cbf146b3aca6c34a0c
Samuel Young <sammy.young@softwire.com>
2022-07-12 10:53:49 +0100

    make edit button open description box

--- todo_app/static/script.js
+++ todo_app/static/script.js
@@ -3,5 +3,11 @@ $("#btn-add").mouseup(() => {
 });
 
 function edit_box(id) {
-    alert(id)
+    const task = $("#item62cc21a4b85ea25d99fcf2b9");
+
+    const description = task.find(".description");
+    description.removeClass("disabled");
+    description.removeAttr("disabled");
+
+    const edit = task.find(".edit");
 }

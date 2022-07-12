37b2e5d7195d1d0f640934639b8bbfc00efd8374
Samuel Young <sammy.young@softwire.com>
2022-07-12 13:36:03 +0100

    prepend jquery var names with $ it seems this is standard

--- todo_app/static/script.js
+++ todo_app/static/script.js
@@ -3,21 +3,21 @@ $("#btn-add").mouseup(() => {
 });
 
 function edit_box(id) {
-    const task = $(`#item${id}`);
+    const $task = $(`#item${id}`);
 
-    const description = task.find(".description");
-    description.removeClass("disabled");
-    description.removeAttr("disabled");
+    const $description = $task.find(".description");
+    $description.removeClass("disabled");
+    $description.removeAttr("disabled");
 
-    const date = task.find(".date");
-    date.removeClass("disabled");
-    date.removeAttr("disabled");
+    const $date = $task.find(".date");
+    $date.removeClass("disabled");
+    $date.removeAttr("disabled");
 
-    const edit = task.find(".edit");
-    edit.addClass("d-none");
+    const $edit = $task.find(".edit");
+    $edit.addClass("d-none");
 
-    const submit = task.find(".submit");
-    submit.removeClass("d-none");
+    const $submit = $task.find(".submit");
+    $submit.removeClass("d-none");
 }
 
 $('.item').find(".date").datepicker({

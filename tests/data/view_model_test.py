b5bb3fb3b56a6cce2d1d82871b5689ec4c3dc0bc
Samuel Young <sammy.young@softwire.com>
2022-07-13 09:20:08 +0100

    parametrize the test now can test both get complete & get incomplete with one function

--- tests/data/view_model_test.py
+++ tests/data/view_model_test.py
@@ -1,4 +1,6 @@
-from pytest import fixture
+from typing import Callable
+
+from pytest import fixture, mark
 
 from todo_app.data.task import Task
 from todo_app.data.view_model import ViewModel
@@ -13,13 +15,20 @@ def simple_view_model():
     return view_model
 
 
-def test_complete_items(simple_view_model):
-    complete_tasks = simple_view_model.complete_items()
+test_params = [
+    (ViewModel.complete_items,   "complete",   "incomplete"),
+    (ViewModel.incomplete_items, "incomplete", "complete")
+]
 
-    assert all(task.status == "complete" for task in complete_tasks)
+
+@mark.parametrize("get_tasks, expected, rest", test_params)
+def test_items_status(simple_view_model, get_tasks: Callable[[ViewModel], list[Task]], expected: str, rest: str):
+    tasks = get_tasks(simple_view_model)
+
+    assert all(task.status == expected for task in tasks)
 
     # subtract complete tasks from tasks to get the rest of the tasks
-    incomplete_tasks = [task for task in simple_view_model.tasks if task not in complete_tasks]
+    other_tasks = [task for task in simple_view_model.tasks if task not in tasks]
 
     # so every other task should have status incomplete
-    assert all(task.status == "incomplete" for task in incomplete_tasks)
+    assert all(task.status == rest for task in other_tasks)

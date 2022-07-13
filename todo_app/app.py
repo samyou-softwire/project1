93c64fc5a737241104ee0078dab008899aac3a24
Samuel Young <sammy.young@softwire.com>
2022-07-13 09:36:22 +0100

    move app to create_app function for better testing apparently ?

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,41 +5,42 @@ from todo_app.data.view_model import ViewModel
 from todo_app.flask_config import Config
 from dateutil import parser as dateparser
 
-app = Flask(__name__)
-app.config.from_object(Config())
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-@app.route('/')
-def index():
-    tasks_view_model = ViewModel(get_tasks())
+    @app.route('/')
+    def index():
+        tasks_view_model = ViewModel(get_tasks())
 
-    return render_template("templates/index.html", view_model=tasks_view_model)
+        return render_template("index.html", view_model=tasks_view_model)
 
+    @app.route('/add', methods=['POST'])
+    def add():
+        add_task(request.form.get("title"))
+        return redirect(url_for("index"))
 
-@app.route('/add', methods=['POST'])
-def add():
-    add_task(request.form.get("title"))
-    return redirect(url_for("index"))
+    @app.route('/update', methods=['POST'])
+    def update():
+        task = get_task(request.form.get("id"))
+        submit_type = request.form.get("submittype")
 
+        if submit_type == "changestatus":
+            task.status = toggle(request.form.get("status"))
 
-@app.route('/update', methods=['POST'])
-def update():
-    task = get_task(request.form.get("id"))
-    submit_type = request.form.get("submittype")
+        if submit_type in ["changestatus", "update"]:
+            task.description = request.form.get("description")
 
-    if submit_type == "changestatus":
-        task.status = toggle(request.form.get("status"))
+            due = request.form.get("due")
+            if due == "":
+                due = None
+            task.due = dateparser.parse(due) if due is not None else None
+            save_task(task)
 
-    if submit_type in ["changestatus", "update"]:
-        task.description = request.form.get("description")
+        if submit_type == "delete" is not None:
+            delete_task(task.id)
 
-        due = request.form.get("due")
-        if due == "":
-            due = None
-        task.due = dateparser.parse(due) if due is not None else None
-        save_task(task)
+        return redirect(url_for("index"))
 
-    if submit_type == "delete" is not None:
-        delete_task(task.id)
-
-    return redirect(url_for("index"))
+    return app

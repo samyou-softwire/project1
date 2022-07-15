8e9e2a9baa3cfaa6c93000ffb170d97be5fe4bdc
Samuel Young <sammy.young@softwire.com>
2022-07-15 10:52:44 +0100

    add a selenium tests to check for deleting items

--- selenium_tests/app_test.py
+++ selenium_tests/app_test.py
@@ -6,6 +6,7 @@ from _pytest.monkeypatch import MonkeyPatch
 from requests import post, delete
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 from todo_app import app
@@ -62,6 +63,13 @@ def driver():
         yield driver
 
 
+def add_task(name, driver):
+    add_name = driver.find_element(By.ID, "itemtitle")
+    add_name.send_keys(name)
+    add_button = driver.find_element(By.ID, "btn-add")
+    add_button.click()
+
+
 def test_app_loads(driver, app_with_temp_board):
     driver.get("http://localhost:5000")
 
@@ -71,10 +79,48 @@ def test_app_loads(driver, app_with_temp_board):
 def test_add_element(driver, app_with_temp_board):
     driver.get("http://localhost:5000")
 
-    add_name = driver.find_element(By.ID, "itemtitle")
-    add_name.send_keys("This is a hard task")
-
-    add_button = driver.find_element(By.ID, "btn-add")
-    add_button.click()
+    add_task("This is a hard task", driver)
 
     assert "This is a hard task" in driver.page_source
+
+
+def test_delete_element(driver, app_with_temp_board):
+    delete_me = "Please delete this task"
+
+    driver.get("http://localhost:5000")
+
+    add_task(delete_me, driver)
+
+    assert delete_me in driver.page_source
+
+    renamed_label = driver.find_element(By.NAME, delete_me)
+    parent: WebElement = renamed_label.find_element(By.XPATH, "./..")
+    delete_button = parent.find_element(By.NAME, "delete")
+    delete_button.click()
+
+    assert delete_me not in driver.page_source
+
+
+def test_delete_element_with_others_on_page(driver, app_with_temp_board):
+    dont_delete_me = "Please DO NOT delete this task"
+    delete_me = "Please delete this task"
+    dont_delete_me2 = "Also please DO NOT delete this task either"
+
+    driver.get("http://localhost:5000")
+
+    add_task(delete_me, driver)
+    add_task(dont_delete_me, driver)
+    add_task(dont_delete_me2, driver)
+
+    assert delete_me in driver.page_source
+    assert dont_delete_me in driver.page_source
+    assert dont_delete_me2 in driver.page_source
+
+    renamed_label = driver.find_element(By.NAME, delete_me)
+    parent: WebElement = renamed_label.find_element(By.XPATH, "./..")
+    delete_button = parent.find_element(By.NAME, "delete")
+    delete_button.click()
+
+    assert delete_me not in driver.page_source
+    assert dont_delete_me in driver.page_source
+    assert dont_delete_me2 in driver.page_source
